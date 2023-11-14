f7830a53d2960e0dd5dae427b48673896e072a4a
Kevin D. Mortensen <kevi5221@edu.zealand.dk>
2023-11-14 12:25:30 +0100

    confirmation page oprettet
    confirm page efter oprettet pakke

--- routes/sendpakke.js
+++ routes/sendpakke.js
@@ -11,7 +11,7 @@ router.post('/', async (req, res) => {
     const { sName, sAddress, sEmail, sPhone, rName, rAddress, rEmail, rPhone, size, weight } = req.body;
     try {
       const packageID = await db.insertPackage(sName, sAddress, sEmail, sPhone, rName, rAddress, rEmail, rPhone, size, weight);
-      res.json({ message: 'Dit ID er :', id: packageID });
+      res.render('confirmation', { title: 'Confirmation', packageID: packageID });
     } catch (err) {
       res.status(500).json({ error: err.message });
     }
