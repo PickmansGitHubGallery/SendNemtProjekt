7fb2e30b1073416485a328394a012d81eb2c2e42
Jakob F <Jakobfoersom88@gmail.com>
2023-11-16 13:44:02 +0100

    3 delt form
    Vi har lavet en 3 delt form til sendpakke siden.
    Den virker ikke endnu, for den sender kun en tom post request til sendpakke.js

--- routes/sendpakke.js
+++ routes/sendpakke.js
@@ -1,6 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const bodyParser = require('body-parser');
+const router = express.Router();
 const db = require('../database/db.js');
+
+// Use bodyParser middleware
+router.use(bodyParser.urlencoded({ extended: true }));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -8,13 +12,14 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', async (req, res) => {
-    const { sName, sAddress, sEmail, sPhone, rName, rAddress, rEmail, rPhone, size, weight } = req.body;
-    try {
-      const packageID = await db.insertPackage(sName, sAddress, sEmail, sPhone, rName, rAddress, rEmail, rPhone, size, weight);
-      res.render('confirmation', { title: 'Confirmation', packageID: packageID });
-    } catch (err) {
-      res.status(500).json({ error: err.message });
-    }
-  });
+  console.log(req.body);
+  const { sName, sAddress, sEmail, sPhone, rName, rAddress, rEmail, rPhone, size, weight } = req.body;
+  try {
+    const packageID = await db.insertPackage(sName, sAddress, sEmail, sPhone, rName, rAddress, rEmail, rPhone, size, weight);
+    res.render('confirmation', { title: 'Confirmation', packageID: packageID });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 
 module.exports = router;
