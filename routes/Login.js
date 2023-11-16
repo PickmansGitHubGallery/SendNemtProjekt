33ec5489e4b4d55c2e824ade82a62c1029402e1e
Kevin D. Mortensen <kevi5221@edu.zealand.dk>
2023-11-16 12:20:05 +0100

    Login authentication + cookies er lavet

--- routes/Login.js
+++ routes/Login.js
@@ -4,20 +4,39 @@ const db = require('../database/db.js');
 
 /*GET login page. */
 router.get('/', function(req, res, next) {
-    res.render('login', { title: 'Login' });
+    let token = req.cookies.token;
+  if (token) {
+    db.authenticateToken(token)
+      .then((userData) => {
+        res.render('index', { title: 'Home', userData: userData });
+      })
+      .catch((err) => {
+        console.error('Error while authenticating token:', err);
+        res.status(401).send('Unauthorized');
+      });
+  }
+  else {
+    res.render('Login', { title: 'Login' });
+  }
 });
 
 router.post('/', function(req, res, next) {
-    console.log("her" + req.body.email);
     db.authenticateUser(req.body.email, req.body.password)
         .then((userData) => {
-            console.log("Du logger ind", userData)
+            db.setAuthenticationToken(userData.email)
+                .then((tokenhash) =>{
+                    res.cookie('token', tokenhash, { maxAge: 9000000, path: '/', domain: 'localhost' });
+                    res.redirect('/')
+                    })
+                .catch((err) => {
+                    console.error('Internal Error:', err);
+                    res.status(500).send('Internal Server Error');
+                    });
         })
         .catch((err) => {
             console.error("Du er ikke logget ind", err.message)
         }) 
-
-});
+    });
 
 module.exports = router;
   
