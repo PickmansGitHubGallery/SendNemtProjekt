3b6e89a7b3cc31d294bb88afc83475cc7b87daad
emil51z8 <emil51z8@edu.zealand.dk>
2023-11-16 13:01:54 +0100

    implemtering af cookies til statistic

--- routes/statistic.js
+++ routes/statistic.js
@@ -6,28 +6,32 @@ const db = require('../database/db.js');
 
 /* GET statistic page. */
 router.get('/', function(req, res, next) {
-  let user ="";
-  db.getUserInfo("hej@gmail.com")
-  .then((userdata) => {
-    user = userdata;
-  })
-  .catch((err) => {
-    console.error("Du er ikke logget ind", err.message)
-})    
-  
-  
-  db.getAllPackages("kevin.dyhr@gmail.com") //TODO: Change to req.session.email
+  let user = " ";
+  let token = req.cookies.token;
+  if (token) {
+    db.authenticateToken(token)
+  .then((userData) => {
+    user = userData;
+    db.getAllPackages(user.email)
   .then((packages) => {
-    console.log(user);
     res.render('statistic', { title: 'Min side', packages: packages, user: user});
   })
   .catch((err) => {
     console.error("Du er ikke logget ind", err.message)
 }) 
+  })
+  .catch((err) => {
+    res.redirect('/login');
+    console.log(err.message);
+})    
+  }
+  if(!token){
+    res.redirect('/login');
+  }
 });
 
 router.post('/', function(req, res, next) {
-  db.updateUserDetails("hej@gmail.com",req.body.name,req.body.address,req.body.phone)
+  db.updateUserDetails(req.body.email ,req.body.name,req.body.phone,req.body.address)
   .then(() => {
     console.log("Du har opdateret dine oplysninger")
 })
